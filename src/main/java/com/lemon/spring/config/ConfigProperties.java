ad41636fbe551abedcc062bb980a8941cb135271
khirulbasher <khirulbasherbsmrstu@gmail.com>
2018-10-02 14:42:42 +0600

    Config Props Updated

--- src/main/java/com/lemon/spring/config/ConfigProperties.java
+++ src/main/java/com/lemon/spring/config/ConfigProperties.java
@@ -13,26 +13,74 @@ import org.springframework.context.annotation.Configuration;
 @EnableConfigurationProperties
 @ConfigurationProperties
 public class ConfigProperties {
-    private GeneralSettings generalSettings;
 
-
-    public static class GeneralSettings {
-        private String name;
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
+    public static class Database {
+        private String url;
+        private String schema;
+        private String username;
+        private String password;
+        private boolean enableStatistics=false;
     }
 
-    public GeneralSettings getGeneralSettings() {
-        return generalSettings;
-    }
+    public static class Settings {
+        private General general;
+        private Security security;
 
-    public void setGeneralSettings(GeneralSettings generalSettings) {
-        this.generalSettings = generalSettings;
+        public Security getSecurity() {
+            return security;
+        }
+
+        public void setSecurity(Security security) {
+            this.security = security;
+        }
+
+        public General getGeneral() {
+            return general;
+        }
+
+        public void setGeneral(General general) {
+            this.general = general;
+        }
+        public static class General {
+            private String name;
+
+            public String getName() {
+                return name;
+            }
+
+            public void setName(String name) {
+                this.name = name;
+            }
+        }
+
+        public static class Security{
+            private String secretKey="MY_SECRET_KEY";
+            private String enableRememberMe;
+            private Double sessionLiveTime;
+
+            public String getSecretKey() {
+                return secretKey;
+            }
+
+            public void setSecretKey(String secretKey) {
+                this.secretKey = secretKey;
+            }
+
+            public String getEnableRememberMe() {
+                return enableRememberMe;
+            }
+
+            public void setEnableRememberMe(String enableRememberMe) {
+                this.enableRememberMe = enableRememberMe;
+            }
+
+            public Double getSessionLiveTime() {
+                return sessionLiveTime;
+            }
+
+            public void setSessionLiveTime(Double sessionLiveTime) {
+                this.sessionLiveTime = sessionLiveTime;
+            }
+        }
     }
 }
