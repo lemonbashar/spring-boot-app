fd143d9733dbacfc3d19a10053afa64fa78e192c
khirulbasher <khirulbasherbsmrstu@gmail.com>
2018-10-21 11:45:36 +0600

    Hib Added

---
+++ src/main/java/com/lemon/spring/config/database/HibernateConfig.java
@@ -0,0 +1,14 @@
+package com.lemon.spring.config.database;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by lemon on 10/21/18.
+ */
+
+@SuppressWarnings({"unused", "DefaultFileTemplate"})
+public class HibernateConfig {
+    public AnnotationSessionFactoryBean localSessionFactoryBean(DataSource dataSource) {
+
+    }
+}
