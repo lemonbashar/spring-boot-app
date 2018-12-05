3b8d6b58de7f4b638feaa62955fe081a54d575da
khirulbasher <khirulbasherbsmrstu@gmail.com>
2018-12-05 20:49:47 +0600

    Updated

--- src/main/java/com/lemon/spring/repository/UserRepository.java
+++ src/main/java/com/lemon/spring/repository/UserRepository.java
@@ -1,32 +1,12 @@
 package com.lemon.spring.repository;
 
 import com.lemon.spring.domain.User;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 
 @Repository
 @Transactional
 public interface UserRepository extends JpaRepository<User,Long> {
-
-    @Cacheable
-    @Override
-    Optional<User> findById(Long aLong);
-
-    @Override
-    void deleteById(Long aLong);
-
-    @Override
-    void delete(User entity);
-
-    @Override
-    void deleteAll();
-
-    @Override
-    <S extends User> Optional<S> findOne(Example<S> example) ;
 }
