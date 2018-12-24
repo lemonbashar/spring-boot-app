fb4851f0325732043aac3c8fe65498c94a0a7e59
khirulbasher <khirulbasher@gmail.com>
2018-12-24 18:48:17 +0600

    Spring-Boot: Global Method Security Added

--- src/main/java/com/lemon/spring/config/security/MethodSecurityConfig.java
+++ src/main/java/com/lemon/spring/config/security/MethodSecurityConfig.java
@@ -1,11 +1,19 @@
 package com.lemon.spring.config.security;
 
+import com.lemon.spring.service.security.method.GlobalPermissionEvaluator;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 
 @Configuration
-@EnableGlobalMethodSecurity(securedEnabled = true)
+@EnableGlobalMethodSecurity(securedEnabled = true,prePostEnabled = true,jsr250Enabled = true)
 public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
-
+    @Override
+    protected MethodSecurityExpressionHandler createExpressionHandler() {
+        DefaultMethodSecurityExpressionHandler expressionHandler = new DefaultMethodSecurityExpressionHandler();
+        expressionHandler.setPermissionEvaluator(new GlobalPermissionEvaluator());
+        return expressionHandler;
+    }
 }
---
+++ src/main/java/com/lemon/spring/service/security/method/GlobalPermissionEvaluator.java
@@ -0,0 +1,32 @@
+package com.lemon.spring.service.security.method;
+
+import com.lemon.spring.domain.Authority;
+import org.springframework.security.access.PermissionEvaluator;
+import org.springframework.security.core.Authentication;
+
+import java.io.Serializable;
+
+@SuppressWarnings("WeakerAccess")
+public class GlobalPermissionEvaluator implements PermissionEvaluator {
+    public static final String READ="READ";
+    public static final String WRITE="WRITE";
+
+    /**
+     * permission is the permission type of like hasPermission(,'READ')
+     * */
+    @Override
+    public boolean hasPermission(Authentication authentication, Object target, Object permission) {
+        switch (permission.toString()) {
+            case READ:
+                return true;
+            case WRITE:
+                return target instanceof Authority;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean hasPermission(Authentication authentication, Serializable targetId, String targetType, Object permission) {
+        return false;
+    }
+}
