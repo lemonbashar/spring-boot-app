ef219adc4001ae7f0c50b5e62cf437263166bdd3
khirulbasher <khirulbasherbsmrstu@gmail.com>
2019-01-05 01:05:15 +0600

    RMI-Successfully Included

---
+++ src/main/java/com/lemon/spring/config/remote/RemoteConfig.java
@@ -0,0 +1,5 @@
+package com.lemon.spring.config.remote;
+
+public class RemoteConfig {
+    public static final int RMI_PORT=124;
+}
---
+++ src/main/java/com/lemon/spring/config/remote/rmi/exporter/AccountExporter.java
@@ -0,0 +1,22 @@
+package com.lemon.spring.config.remote.rmi.exporter;
+
+import com.lemon.framework.springremote.rmi.RmiUserService;
+import com.lemon.spring.config.remote.RemoteConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.remoting.rmi.RmiServiceExporter;
+
+@Configuration
+public class AccountExporter {
+
+    @Bean
+    public RmiServiceExporter userServiceExport(RmiUserService rmiUserService) {
+        Class<RmiUserService> service = RmiUserService.class;
+        RmiServiceExporter serviceExporter=new RmiServiceExporter();
+        serviceExporter.setServiceInterface(service);
+        serviceExporter.setService(rmiUserService);
+        serviceExporter.setServiceName(service.getName());
+        serviceExporter.setRegistryPort(RemoteConfig.RMI_PORT);
+        return serviceExporter;
+    }
+}
