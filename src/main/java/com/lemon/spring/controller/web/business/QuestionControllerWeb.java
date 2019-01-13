ded1cf99e55f8f322e8d0284a59be109e2d030af
khirulbasher <khirulbasherbsmrstu@gmail.com>
2019-01-13 23:34:07 +0600

    Updated

--- src/main/java/com/lemon/spring/controller/web/business/QuestionControllerWeb.java
+++
@@ -1,9 +0,0 @@
-package com.lemon.spring.controller.web.business;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping(value = "/web")
-public class QuestionControllerWeb {
-}
