25686a3cad713bb14d31216d58567ac75eea272a
khirulbasher <khirulbasherbsmrstu@gmail.com>
2019-01-22 21:39:03 +0600

    Mail Config Updated

--- src/main/java/com/lemon/spring/config/service/MailConfig.java
+++ src/main/java/com/lemon/spring/config/service/MailConfig.java
@@ -36,21 +36,4 @@ public class MailConfig {
         return mailSender;
     }
 
-    //@Bean
-    public JavaMailSender mailSender2() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
-
-        mailSender.setUsername(properties.settings.mail.username);
-        mailSender.setPassword(properties.settings.mail.password);
-
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
-
-        return mailSender;
-    }
 }
