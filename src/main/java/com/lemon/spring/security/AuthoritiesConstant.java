c44e9d59c2822ad1adf97d584d9baea4f12570c5
khirulbasher <khirulbasherbsmrstu@gmail.com>
2019-02-26 09:51:22 +0600

    Authority Updated

--- src/main/java/com/lemon/spring/security/AuthoritiesConstant.java
+++ src/main/java/com/lemon/spring/security/AuthoritiesConstant.java
@@ -4,6 +4,6 @@ public interface AuthoritiesConstant {
     String ROLE_ADMIN="ROLE_ADMIN";
     String ROLE_USER="ROLE_USER";
 
-    /*Collection Authorities*/
+    /*Collection Of Authorities*/
     String[] ROLES_FOR_ADMIN = {ROLE_ADMIN};
 }
