c89071e3c777686f69e36ee97be8e60428e35cb0
khirulbasher <khirulbasherbsmrstu@gmail.com>
2019-04-18 01:50:36 +0600

    SHA-1 Filtering Security Chain Integrated Successfully

--- src/main/java/com/lemon/spring/config/Constants.java
+++ src/main/java/com/lemon/spring/config/Constants.java
@@ -5,6 +5,7 @@ public final class Constants {
 
     public static final String PROFILE_DEVELOPMENT = "dev";
     public static final String PROFILE_PRODUCTION = "prod";
+    public static final String PROFILE_X_SECURE = "xsecure";
     public static final String PROFILE_FAST = "fast";
     public static final String PROFILE_CLOUD = "cloud";
 
---
+++ src/main/java/com/lemon/spring/config/security/FilterRegistration.java
@@ -0,0 +1,22 @@
+package com.lemon.spring.config.security;
+
+import com.lemon.framework.security.auth.AuthorizationBridge;
+import com.lemon.framework.springsecurity.filter.sha.SHA1Filter;
+import com.lemon.spring.config.Constants;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class FilterRegistration {
+
+    @Profile(Constants.PROFILE_X_SECURE)
+    @Bean
+    @Order(1)
+    public Filter sha1Filter(AuthorizationBridge authorizationBridge) {
+        return new SHA1Filter(authorizationBridge);
+    }
+}
