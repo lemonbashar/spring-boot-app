d87595185b552e11243bfd2e3bd298c27e647c1d
khirulbasher <khirulbasherbsmrstu@gmail.com>
2019-05-03 12:35:26 +0600

    Updated

--- src/main/java/com/lemon/spring/config/docs/SwaggerApiConfig.java
+++ src/main/java/com/lemon/spring/config/docs/SwaggerApiConfig.java
@@ -1,13 +1,16 @@
 package com.lemon.spring.config.docs;
 
+import com.lemon.spring.config.Constants;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@Profile(Constants.PROFILE_DEVELOPMENT)
 @Configuration
 @EnableSwagger2
 public class SwaggerApiConfig {
